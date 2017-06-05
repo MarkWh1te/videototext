0faa24be9b2deb3f01b5e02452a34a29d3013641
markwh1te <iamwh1temark@outlook.com>
2017-06-04 23:57:42 -0700

    first try

--- mp4_to_audio.py
+++ mp4_to_audio.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 import moviepy.editor as mp
-clip = mp.VideoFileClip("myvideo.mp4").subclip(0,20)
-clip.audio.write_audiofile("theaudio.mp3")
+clip = mp.VideoFileClip("sample_data/test.mp4").subclip(0,20)
+clip.audio.write_audiofile("test.mp3")
